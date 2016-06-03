45df5e29cfa44e956c37c6cb9826e2172805433a
Doge Zhou <doge.zhou@outlook.com>
2016-06-03 18:30:18 +0800

    fix bugs

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,8 +17,8 @@ from flask import render_template, request, redirect, url_for, flash
 def before_request():
     if current_user.is_authenticated:
         current_user.ping()
-        if not current_user.confirmed \
-                and request.endpoint[:5] != 'auth.':
+        if not current_user.confirmed:
+                # and request.endpoint[:5] != 'auth.':
             return redirect(url_for('auth.unconfirmed'))
 
 
